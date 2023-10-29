d597d8a7a9ea77d0d02cfb5e905a8c2f28dad46b
Dave Naugler <davenaugler@gmail.com>
2023-10-29 17:55:20 -0600

    committing to get off of clean dev branch

--- src/test/java/com/coderscampus/assignment7/CustomArrayListTest.java
+++ src/test/java/com/coderscampus/assignment7/CustomArrayListTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class CustomArrayListDiffblueTest {
+class CustomArrayListTest {
     /**
      * Method under test: {@link CustomArrayList#toString()}
      */
