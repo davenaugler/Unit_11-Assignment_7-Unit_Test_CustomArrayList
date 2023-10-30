6bb89296071e5532f3c304d3e79c25f234d25a9d
Dave Naugler <davenaugler@gmail.com>
2023-10-29 19:10:07 -0600

    adding more System.out.println(); on main for testing purposes

--- src/com/coderscampus/assignment7/CustomListApplication.java
+++ src/com/coderscampus/assignment7/CustomListApplication.java
@@ -36,52 +36,15 @@ public class CustomListApplication {
 		list.add(40, 42);
 		System.out.println("Added 42 at index 40.");
 		list.add(41);
-		System.out.println("Added 41 at the end.");
+		System.out.println("Added 41 to end of list.");
+		System.out.println("Size of list: " + list.getSize() + " | " + " LIST: " + list);
 	}
 
 	private static void testRemoveFunction(CustomList<Integer> list) {
 		System.out.println("--------");
 		System.out.println("Removed item at index 5: " + list.remove(5));
-		System.out.println("Updated size: " + list.getSize());
+		System.out.println("Updated size: " + list.getSize() + " | " + " LIST: " + list);
 	}
-
-
-
-
-
-
-
-//		CustomList<Integer> myCustomList = new CustomArrayList<>();
-////		myCustomList.add("element 1");  and continue to add another 10, 20 or 40 more elements
-//
-////		for (int i = 1; i < 101; i++) {
-////			myCustomList.add(i);
-////		}
-//
-//
-//		for (int i = 1; i < 41; i++) {
-//			myCustomList.add(i);
-//		}
-//
-//		myCustomList.add(40, 42);
-//		System.out.println("LIST IS: " + myCustomList);
-//		myCustomList.add(41);
-//		System.out.println("LIST IS THROW: " + myCustomList);
-//
-//
-//
-//
-//		System.out.println("getSize: " + myCustomList.getSize());
-//		System.out.println("get: " + myCustomList.get(0));
-//		System.out.println("--------");
-//
-//		// then you should validate that all the elements you've inserted actually exist
-//		// in your data structure
-//		for (int i = 0; i < myCustomList.getSize(); i++) {
-//			System.out.println(myCustomList.get(i));
-//		}
-//		System.out.println("Removed item: " + myCustomList.remove(5));
-//		System.out.println("getSize: " + myCustomList.getSize());
 
 }
 
